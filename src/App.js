205e58df9f70081006ad94370fa68bcec6690664
wtsh0 <t.ueno0207@gmail.com>
2018-09-09 14:17:25 +0900

    learned eventHandler.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import './App.css';
 import Title from './components/Title';
 
 class App extends Component {
+
+  onClickHandler = () => {
+    // HelloWorld のh2タグを取得
+    let title = document.getElementById('versionStatement');
+    // upgrade のタグを取得
+    let upgradeButton = document.getElementById('upgradeButton');
+    // 取得したh2タグのテキストを変更する
+    title.textContent = "Hello World 4.0";
+    // 取得したpタグを非表示にする
+    upgradeButton.style.display = "none";
+
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Title title="Hello World 3.0" titleStyle={{ color: 'orange' }} />
+          <Title
+            title="Hello World 3.0"
+            titleStyle={{ color: 'orange' }}
+            onClick={this.onClickHandler}
+          />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Title = (props) => {
   return (
-    <div>
-      <h2 style={props.titleStyle}>{props.title}</h2>
+    <div className="titleSelection">
+      <h2
+        style={props.titleStyle}
+        id="versionStatement"
+      >
+        {props.title}
+      </h2>
+      <p
+        onClick={props.onClick}
+        id="upgradeButton"
+      >
+        Upgrade
+      </p>
     </div>
   )
 }
